811bfc5b3c3ab9f0145a66953ab8e1a3493ba1bd
popark05 <popark05@gmail.com>
2024-05-12 13:20:52 +0900

    feat: change PostController.kt (와일드카드 제)

--- src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
+++ src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
@@ -3,13 +3,20 @@ package com.fastcampus.fcboard.controller
 import com.fastcampus.fcboard.controller.dto.PostCreateRequest
 import com.fastcampus.fcboard.controller.dto.PostDetailResponse
 import com.fastcampus.fcboard.controller.dto.PostSearchRequest
-import com.fastcampus.fcboard.controller.dto.PostUpdateRequest
 import com.fastcampus.fcboard.controller.dto.PostSummaryResponse
+import com.fastcampus.fcboard.controller.dto.PostUpdateRequest
 import org.springdoc.core.parsers.ReturnTypeParser
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
