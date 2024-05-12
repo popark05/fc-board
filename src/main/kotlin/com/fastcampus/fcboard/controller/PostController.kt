71ead5c92c3dd09499c3297dad874775949a495e
popark05 <popark05@gmail.com>
2024-05-12 13:12:10 +0900

    feat: change PostController.kt

--- src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
+++ src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
@@ -1,6 +1,11 @@
 package com.fastcampus.fcboard.controller
 
-import com.fastcampus.fcboard.controller.dto.*
+import com.fastcampus.fcboard.controller.dto.PostCreateRequest
+import com.fastcampus.fcboard.controller.dto.PostDetailResponse
+import com.fastcampus.fcboard.controller.dto.PostSearchRequest
+import com.fastcampus.fcboard.controller.dto.PostUpdateRequest
+import com.fastcampus.fcboard.controller.dto.PostSummaryResponse
+
 import org.springdoc.core.parsers.ReturnTypeParser
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.domain.Page
